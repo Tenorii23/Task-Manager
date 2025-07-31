dec4a42f11e7138bcac7de587e9fed085b198e0f
Tenorii23 <tenor.sopa@gmail.com>
2025-07-31 16:43:15 +0200

    BETA

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Task Manager</title>
+</head>
+<body>
+  <h1>Coming Soon...</h1>
+</body>
+</html>
+
 <?php
 include 'config.php';
 
@@ -11,3 +21,4 @@ while ($row = $result->fetch_assoc()) {
 }
 ?>
 <a href="logout.php">Logout</a>
+
